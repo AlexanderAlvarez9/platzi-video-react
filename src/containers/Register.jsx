fff9166dd698b5a036d086cad66755c8948967df
Alexander <jhonalexander9@gmail.com>
2020-09-10 21:22:35 -0500

    add routes styles and functionality

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/styles/components/Register.scss';
 import { Link } from 'react-router-dom';
 
-const Register = () => (
-  <section class="register">
-    <section class="register__container">
-      <h2>Regístrate</h2>
-      <form class="register__container--form">
-        <input class="input" type="text" placeholder="Nombre" />
-        <input class="input" type="text" placeholder="Correo" />
-        <input class="input" type="password" placeholder="Contraseña" />
-        <button class="button">Registrarme</button>
-      </form>
-      <Link to="/login">
-        Iniciar Sesion
+const Register = () => {
+  const [form, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+
+  const handleInput = e => {
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(form);
+  }
+
+  return (
+    <section className="register">
+      <section className="register__container">
+        <h2>Regístrate</h2>
+        <form className="register__container--form" onSubmit={handleSubmit}>
+          <input
+            name="name"
+            className="input"
+            type="text"
+            placeholder="Nombre"
+          />
+          <input
+            name="email"
+            className="input"
+            type="text"
+            placeholder="Correo"
+            onChange={handleInput}
+          />
+          <input
+            name="password"
+            className="input"
+            type="password"
+            placeholder="Contraseña"
+            onChange={handleInput}
+          />
+          <button className="button">Registrarme</button>
+        </form>
+        <Link to="/login">
+          Iniciar Sesion
       </Link>
+      </section>
     </section>
-  </section>
-)
+  )
+}
 
 export default Register;
